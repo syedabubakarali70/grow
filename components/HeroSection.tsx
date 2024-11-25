2faff9abb695842b126254fe4f347c8960fb6afb
Syed Abubakar Ali <syedabubakarali70@gmail.com>
2024-11-25 07:34:29 +0500

    Added favicon in tab

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="w-full bg-darkblue relative">
       <div className="hidden absolute w-full right-0 top-1 md:flex justify-end ">
-        <Image src="/logo-lime.svg" width={300} height={100} alt="logo" />
+        <Image src="/favicon/favicon.svg" width={300} height={100} alt="logo" />
       </div>
       <div className="custom-container-md pt-10 md:pt-40 pb-10 flex flex-col gap-4 items-start">
         {children}
