edd833c8714582a53154dbad8c807d7f4dec97dc
Syed Abubakar Ali <syedabubakarali70@gmail.com>
2024-11-26 21:45:48 +0500

    Added tags in search field

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -2,38 +2,70 @@
 import { Search, X } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const SearchBox = ({ children }: { children: React.ReactNode }) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [inputValue, setInputValue] = useState("");
-  const [clear, setClear] = useState(false); // State to control the `x` icon visibility
+  // const [clear, setClear] = useState(false); // State to control the `x` icon visibility
+  const [tags, setTags] = useState<string[]>([]);
+  // const handleClear = () => {
+  //   setInputValue(""); // Clear the input field
+  //   setClear(false); // Hide the `x` icon
+  // };
 
-  const handleClear = () => {
-    setInputValue(""); // Clear the input field
-    setClear(false); // Hide the `x` icon
+  const handleClearTag = (tag: string) => {
+    setTags(tags.filter(prevTag => prevTag != tag));
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-    setClear(e.target.value.length > 0); // Show `x` icon only if input has text
+    // setClear(e.target.value.length > 0); // Show `x` icon only if input has text
+  };
+
+  const handleSearchButton = () => {
+    if (inputValue.trim()) {
+      setTags([...tags, inputValue]);
+      setInputValue("");
+    }
+    inputRef.current?.focus();
   };
   return (
     <div className="w-full flex justify-center flex-col gap-4 py-8 bg-white px-4 md:px-12 shadow-md  rounded-md">
       {children}
-      <div className="border-input border p-1 rounded-md flex gap-1">
+      <div
+        className="border-input border p-1 rounded-md flex gap-1 overflow-x-auto"
+        id="search-input"
+      >
+        {tags.map((tag, index) => (
+          <div key={index} className="pl-2 flex gap-1 items-center">
+            <p> {tag}</p>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => handleClearTag(tag)}
+            >
+              <X />
+            </Button>
+          </div>
+        ))}
         <Input
-          className="border-0 focus-visible:ring-0 shadow-none"
+          ref={inputRef}
+          className="border-0 focus-visible:ring-0 shadow-none min-w-20"
           value={inputValue}
           onChange={handleChange}
           placeholder="Enter keywords to search for resources"
         />
-        {clear && (
+        {/* {clear && (
           <Button onClick={handleClear} variant="ghost" size="icon">
             {" "}
             <X />
           </Button>
-        )}
-        <Button className="text-white font-poppinsExtraBold">
+        )} */}
+        <Button
+          className="text-white font-poppinsExtraBold"
+          onClick={handleSearchButton}
+        >
           <Search strokeWidth={4} /> Search
         </Button>
       </div>
